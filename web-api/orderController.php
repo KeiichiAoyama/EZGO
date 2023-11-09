c177680f6a79fb10521c0f0bb82ecc14600b8c15
ZinedineZethro <zinedine.zethro@gmail.com>
2023-11-09 14:36:08 +0700

    change methods

--- web-api/orderController.php
+++ web-api/orderController.php
@@ -127,6 +127,71 @@ class order{
         $json = file_get_contents('php://input');
 
         $request = json_decode($json, true);
+        $tix = request['ticket'];
+
+        $fields = array('tcSeat');
+        $values = array($tix->tcSeat);
+        $assoc_array = array_combine($fields, $values);
+        
+        $where = array('ticketID', $tix->ticketID);
+
+        if($db->update('tickets', $assoc_array, $where)){
+            $response = ['Success'=> True];
+            header('Content-Type: application/json');
+            echo json_encode($response);
+        }else{
+            $response = ['Success'=> False];
+            header('Content-Type: application/json');
+            echo json_encode($response);
+        }
+    }
+
+    public function changeHotel(){
+        $db = DB::getInstance();
+        $json = file_get_contents('php://input');
+
+        $request = json_decode($json, true);
+        $htl = request['hotel'];
+
+        $fields = array('hNights');
+        $values = array($htl->hNights);
+        $assoc_array = array_combine($fields, $values);
+        
+        $where = array('hotelID', $htl->hotelID);
+
+        if($db->update('hotel', $assoc_array, $where)){
+            $response = ['Success'=> True];
+            header('Content-Type: application/json');
+            echo json_encode($response);
+        }else{
+            $response = ['Success'=> False];
+            header('Content-Type: application/json');
+            echo json_encode($response);
+        }   
+    }
+
+    public function changeTour(){
+        $db = DB::getInstance();
+        $json = file_get_contents('php://input');
+
+        $request = json_decode($json, true);
+        $trp = request['tour'];
+
+        $fields = array('tpSlot');
+        $values = array($trp->tpSlot);
+        $assoc_array = array_combine($fields, $values);
+        
+        $where = array('tourID', $trp->tourID);
+
+        if($db->update('tour_package', $assoc_array, $where)){
+            $response = ['Success'=> True];
+            header('Content-Type: application/json');
+            echo json_encode($response);
+        }else{
+            $response = ['Success'=> False];
+            header('Content-Type: application/json');
+            echo json_encode($response);
+        }   
     }
 }
 

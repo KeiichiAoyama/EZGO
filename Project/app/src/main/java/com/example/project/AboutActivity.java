94b0f56acb88422a56de5c6ca401a2c494007529
Muhammad Ilham Kurniawan <127322172+muhilhamk@users.noreply.github.com>
2023-11-28 22:17:02 +0700

    update front End

--- Project/app/src/main/java/com/example/project/AboutActivity.java
+++ Project/app/src/main/java/com/example/project/AboutActivity.java
@@ -3,6 +3,8 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        FrameLayout btnBack = findViewById(R.id.backAbout);
+        btnBack.setOnClickListener(view -> onBackPressed());
     }
 }
